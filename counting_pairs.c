f5addbcc7924e1ddea7a3ef75dc19b2c79e42f26
sidharth-puri <52946626+sidharth-puri@users.noreply.github.com>
2019-08-05 19:25:33 +0530

    Add files via upload
    counting pairs hackerrank

---
+++ counting_pairs.c
@@ -0,0 +1,157 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int comparator(const int *p,const int *q)
+{
+    return (*p-*q);
+}
+int binary_search(int *arr,int f,int l,int n)
+{
+    if(f<=l){
+    int mid= f+ (l-f)/2;
+    if(arr[mid]==n)
+    
+        return mid;
+    
+    if(arr[mid]>n)
+    
+        return binary_search(arr,f,mid-1,n);
+    
+    else
+    
+        return binary_search(arr,mid+1,l,n);
+    
+    }
+    return -1;
+}
+char* readline();
+char** split_string(char*);
+int pairs(int k, int arr_count, int* arr) {
+int i,j=0;
+for(i=0;i<arr_count;i++)
+{
+    if(arr[i]==arr[j])
+    {
+        continue;
+    }
+    else if(arr[i]!=arr[j])
+    {
+        j++;
+        arr[j]=arr[i];
+    }
+}
+int arl=j+1;
+int count=0;
+qsort(arr,arl,sizeof(int),comparator);
+for(i=0;i<arl;i++)
+{
+    if(binary_search(arr,0,arl-1,arr[i]+k)!=-1)
+    {
+        count++;
+    }
+}
+return count;
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** nk = split_string(readline());
+
+    char* n_endptr;
+    char* n_str = nk[0];
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* k_endptr;
+    char* k_str = nk[1];
+    int k = strtol(k_str, &k_endptr, 10);
+
+    if (k_endptr == k_str || *k_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** arr_temp = split_string(readline());
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        char* arr_item_endptr;
+        char* arr_item_str = *(arr_temp + i);
+        int arr_item = strtol(arr_item_str, &arr_item_endptr, 10);
+
+        if (arr_item_endptr == arr_item_str || *arr_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        *(arr + i) = arr_item;
+    }
+
+    int arr_count = n;
+
+    int result = pairs(k, arr_count, arr);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
