bc564aad2ff164bdf452fd051e91e7f2e3a69f78
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-06-24 21:57:07 +0530

    Update diameter_tree.cpp

--- diameter_tree.cpp
+++ diameter_tree.cpp
@@ -1,8 +1,7 @@
 {
 #include <bits/stdc++.h>
 using namespace std;
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child */
+
 struct Node
 {
     int data;
@@ -14,9 +13,9 @@ struct Node
         left = right = NULL;
     }
 };
-/* Function to get diameter of a binary tree */
+
 int diameter(struct Node * tree);
-/* Driver program to test size function*/
+
 int main()
 {
   int t;
@@ -57,20 +56,7 @@ int main()
 }
 
 }
-/*This is a function problem.You only need to complete the function given below*/
-/* Tree node structure  used in the program
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-}; */
-/* Computes the diameter of binary tree with given root.  */
+
 int height(Node* node){
     if(node==NULL) {
         return 0;
