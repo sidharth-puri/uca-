24e2dfe2e87a463fa1ce90ea7280bdc22940efce
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-09-07 17:35:20 +0530

    Update Bucket_sort.cpp

--- Bucket_sort.cpp
+++ Bucket_sort.cpp
@@ -5,22 +5,15 @@
 using namespace std; 
 
 void bucketSort(float arr[], int n) 
-{ 
-	 
-	vector<float> b[n]; 
-	
-	
+{  
+	vector<float> b[n]; 	
 	for (int i=0; i<n; i++) 
 	{ 
 	int bi = n*arr[i];  
 	b[bi].push_back(arr[i]); 
 	} 
-
-
 	for (int i=0; i<n; i++) 
 	sort(b[i].begin(), b[i].end()); 
-
- 
 	int index = 0; 
 	for (int i = 0; i < n; i++) 
 		for (int j = 0; j < b[i].size(); j++) 
